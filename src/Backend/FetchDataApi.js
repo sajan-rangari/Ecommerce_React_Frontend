7f2dbf48d1d96d5cdeaca7b9fd880e0cde8a46da
Sajan Rangari <sajan_rangari@persistent.com>
2023-01-03 17:10:06 +0530

    change in url of fetchdataapi file

--- src/Backend/FetchDataApi.js
+++ src/Backend/FetchDataApi.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 const FetchDataApi = ({ setCoupens, isChange, url }) => {
   useEffect(() => {
-    fetch(`https://ecommerce_coupens_api.onrender.com/coupens`, {
+    fetch(`https://ecommerce_coupens_api.onrender.com/api/v1/coupens`, {
       mode: "cors",
       headers: {
         Accept: "application/json",
